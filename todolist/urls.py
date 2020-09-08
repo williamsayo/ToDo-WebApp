6591edf9943b6d84bb18f7270a27413fe4d6e922
Williams Ayo <58149023+williamsayo@users.noreply.github.com>
2020-09-08 20:17:02 +0100

    Add files via upload

---
+++ todolist/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from todoapp.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('todo/',include('todoapp.urls')),
+]
